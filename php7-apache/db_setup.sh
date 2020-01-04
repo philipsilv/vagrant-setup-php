6e3dd5b6263255c191dba00b6674b96761e03318
Luis Felipe <luisfelipelimadasilva@gmail.com>
2020-01-03 22:32:00 -0300

    fix script to create the user vagrant in db

--- php7-apache/db_setup.sh
+++ php7-apache/db_setup.sh
@@ -13,4 +13,5 @@ sudo apt-get install phppgadmin --assume-yes --force-yes
 
 echo "--- Alreando a senha do usuario postgres ---"
 # sudo -u postgres psql -c "ALTER USER postgres with encrypted password '123';"
-sudo -u postgres psql -c "CREATE USER vagrant WITH PASSWORD 'vagrant'; ALTER ROLE vagrant WITH SUPERUSER;"
+sudo -u postgres psql -c "SELECT 1 FROM pg_user WHERE usename = 'vagrant'" | grep -q 1 || sudo -u postgres psql -c "CREATE USER vagrant WITH PASSWORD 'vagrant';"
+sudo -u postgres psql -c "ALTER ROLE vagrant WITH SUPERUSER;"
